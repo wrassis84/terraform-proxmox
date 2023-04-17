ffe69517230e8d3b997adee1cdbe76c225d0a946
William Rezende <wrassis84@gmail.com>
2023-04-17 19:59:40 -0300

    style: change file names

--- main.tf
+++ main.tf
@@ -67,11 +67,11 @@ resource "proxmox_vm_qemu" "virtual_machines" {
   os_type = "cloud-init"
 
   provisioner "local-exec" {
-  # remote-exec example-> ginigangadharan/terraform-iac-usecases: l1nq.com/nYz5T
-  # TF_VAR doc -> l1nq.com/VgUte
-  # Use Input Variables -> l1nq.com/0H2LZ
-  # multiple commands in local-exec provisioner -> l1nk.dev/R37Sr
-  # This build the inventory file in inventory.yaml.
+    # remote-exec example-> ginigangadharan/terraform-iac-usecases: l1nq.com/nYz5T
+    # TF_VAR doc -> l1nq.com/VgUte
+    # Use Input Variables -> l1nq.com/0H2LZ
+    # multiple commands in local-exec provisioner -> l1nk.dev/R37Sr
+    # This build the inventory file in inventory.yaml.
     on_failure = continue
     when       = create
     command    = <<EOT
@@ -86,7 +86,7 @@ resource "proxmox_vm_qemu" "virtual_machines" {
       echo ansible_python_interpreter=/usr/bin/python3 >> $TF_VAR_inventory
     EOT
   }
-  
+
   provisioner "local-exec" {
     # Clean the inventory file at each destroy time.
     on_failure = continue
